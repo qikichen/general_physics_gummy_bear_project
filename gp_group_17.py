c1f1b2675e4ea4b09646ed318944270289489e08
Qi Nohr Chen <75214052+qikichen@users.noreply.github.com>
2023-04-17 22:16:40 +0100

    Algorithm Prototype pushed
    Developed algorithms

--- gp_group_17.py
+++ gp_group_17.py
@@ -13,7 +13,7 @@ General Physics Project:
         Gummy bears are fully molten, no air in between the gummy bears
         Person falling is cuboid
 
-- Qi Nohr Chen (12.04.2023)
+- Qi Nohr/Mikolaj/Leonor/Hamza/Nick (12.04.2023)
 """
 import numpy as np
 import matplotlib as plt
@@ -86,19 +86,28 @@ def new_velocity(init_velocity, deceleration, time_span):
     """
     new_velocity = init_velocity + deceleration*time_span
     return new_velocity
+
+def height_deformation():
+    """
+    Returns the maximum height of the deformation
+    """
+    return 0
     
 
 def _main_():
     initial_gummy_bear_number = 100 #Number of Gummy Bears
+    time = 0
     
-    fall_height = HEIGHT-height_of_gummy_bear_molten(
-        cylinder_volume(initial_gummy_bear_number))
+    gummy_height = height_of_gummy_bear_molten(cylinder_volume(initial_gummy_bear_number))
+    fall_height = HEIGHT - gummy_height
     initial_velocity= energy_to_velocity(fall_height)
     initial_momentum = momentum(initial_velocity)
     
     #Momenta after contact
     momenta_array = np.array([])
     velocity_array = np.array([])
+    time_array = np.array([])
+    force_array = np.array([])
     np.insert(momenta_array, initial_momentum)
     np.insert(velocity_array, initial_velocity)
 
@@ -106,20 +115,25 @@ def _main_():
     #Algorithm : Numerical Iterative Method
     boolean_femur_break = False
     while(boolean_femur_break == False):
-        time = 0 # Initial Parameter for time
+        time += 0.1
+         # Initial Parameter for time
         decelaration_mass = deceleration() # Remember to input something
 
-        #Temporary variables added to the arrays
+        # Temporary variables added to the arrays
         temp_velocity = new_velocity(decelaration_mass)
         temp_momentum = momentum(temp_velocity)
         np.insert(velocity_array, temp_velocity)
         np.insert(momenta_array, temp_momentum)
 
-        #
+        # Calculate Force
+        np.insert(time_array, time)
+        temp_momentum_rate = rate_of_change_momentum(momenta_array[0], momenta_array[-1], time)
+        np.insert(force_array, temp_momentum_rate)
 
+        if(height_deformation >= gummy_height or temp_momentum*-1 >= FEMUR_BREAKGE ):
+            boolean_femur_break = True
+        
 
-    
-    
     
     
     #momentum final iteratively calcualted 
